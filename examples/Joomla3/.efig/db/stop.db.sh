f9612f6b1cb1b6ab6129fba4c595b1bd037911ac
Dmitry Vapelnik <dvapelnik@gmail.com>
2015-02-02 20:46:54 +0200

    Do not dump date in sql-dump-file

--- examples/Joomla3/.efig/db/stop.db.sh
+++ examples/Joomla3/.efig/db/stop.db.sh
@@ -2,11 +2,11 @@
 
 if [[ ! -z $E_DB_DUMP && ! -z $E_DB_NAME ]]; then
     echo "Backuping DB"
-    mysqldump -uroot $E_DB_NAME > /db/$E_DB_DUMP;
+    mysqldump -uroot --skip-dump-date $E_DB_NAME > /db/$E_DB_DUMP;
 fi
 
 if [[ ! -z $E_DB_TEST_DUMP && ! -z $E_DB_TEST_NAME ]]; then
     echo "Backuping test DB"
-    mysqldump -uroot $E_DB_TEST_NAME > /db/$E_DB_TEST_DUMP;
+    mysqldump -uroot --skip-dump-date $E_DB_TEST_NAME > /db/$E_DB_TEST_DUMP;
 fi
 
